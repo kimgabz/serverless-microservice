ea67f1f61f77305b8454cc7bda98ac0bc64a7cf7
kimgabiana <gabianz@yahoo.com>
2020-06-12 15:09:00 +0800

    notification service

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -3,22 +3,15 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({ reqion: 'eu-west-1' });
 
 async function sendMail(event, context) {
-  // console.log(event);
-  // return event;
-
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify({ message: 'Hello from sendMail' }),
-  // };
   const record = event.Records[0];
-  // console.log('record processing', record);
+
   const email = JSON.parse(record.body);
   const { subject, body, recipient } = email;
 
   const params = {
-    Source: 'khemgabz@gmail.com',
+    Source: 'kgabiana@mezy.com',
     Destination: {
-      ToAddresses: recipient,
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
--- trade-service/src/util/closeTrade.js
+++ trade-service/src/util/closeTrade.js
@@ -1,6 +1,7 @@
 import AWS from 'aws-sdk';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 export async function closeTrade(trade) {
   const params = {
@@ -15,6 +16,46 @@ export async function closeTrade(trade) {
     },
   };
 
-  const result = await dynamodb.update(params).promise();
-  return result;
+  await dynamodb.update(params).promise();
+
+  const { title, seller, highestBid } = trade;
+  const { amount, bidder } = highestBid;
+
+  if (amount === 0) {
+    await sqs
+      .sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+          subject: 'No bids on your trade item',
+          recipient: seller,
+          body: `better luck next time on "${title}"`,
+        }),
+      })
+      .promise();
+    return;
+  }
+  console.log(`process.env.MAIL_QUEUE_URL ${process.env.MAIL_QUEUE_URL}`);
+  const notifySeller = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'Your item has been sold!',
+        recipient: seller,
+        body: `Your item "${title}" has been sold for $${amount}`,
+      }),
+    })
+    .promise();
+
+  const notifyBidder = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'You got the Trade!',
+        recipient: bidder,
+        body: `You got yourself the "${title}" for $${amount}`,
+      }),
+    })
+    .promise();
+
+  return Promise.all([notifySeller, notifyBidder]);
 }
